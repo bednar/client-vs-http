2cbcf1be65bc21e118a2ad6ab8a81244df412bd0
Jakub Bednar <jakub.bednar@gmail.com>
2019-10-21 12:32:20 +0200

    Generate record

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -15,7 +15,7 @@ func main() {
 	threadsCount := flag.Int("threadsCount", 2000, "how much Thread use to write into InfluxDB")
 	secondsCount := flag.Int("secondsCount", 30, "how long write into InfluxDB")
 	lineProtocolsCount := flag.Int("lineProtocolsCount", 100, "how much data writes in one batch")
-	measurement := fmt.Sprintf("sensor_%d", time.Now().UnixNano())
+	measurementName := fmt.Sprintf("sensor_%d", time.Now().UnixNano())
 
 	blue := color.New(color.FgHiBlue).SprintFunc()
 	green := color.New(color.FgHiGreen).SprintFunc()
@@ -23,7 +23,7 @@ func main() {
 	fmt.Printf("------------- %s -------------", blue("CLIENT_GO"))
 	fmt.Println()
 	fmt.Println()
-	fmt.Println("measurement:        ", measurement)
+	fmt.Println("measurement:        ", measurementName)
 	fmt.Println("threadsCount:       ", *threadsCount)
 	fmt.Println("secondsCount:       ", *secondsCount)
 	fmt.Println("lineProtocolsCount: ", *lineProtocolsCount)
@@ -37,7 +37,7 @@ func main() {
 	start := time.Now()
 
 	for i := 1; i <= *threadsCount; i++ {
-		go doLoad(&wg, i, *secondsCount)
+		go doLoad(&wg, i, measurementName, *secondsCount, *lineProtocolsCount)
 	}
 
 	wg.Wait()
@@ -56,13 +56,20 @@ func main() {
 	fmt.Println("Total time:", time.Since(start))
 }
 
-func doLoad(wg *sync.WaitGroup, id int, secondsCount int) {
+func doLoad(wg *sync.WaitGroup, id int, measurementName string, secondsCount int, lineProtocolsCount int) {
 	defer wg.Done()
 
 	for i := 1; i <= secondsCount; i++ {
 		if id == 1 {
 			fmt.Printf("\rwriting iterations: %v/%v", i, secondsCount)
 		}
+
+		start := i * lineProtocolsCount
+		end := start + lineProtocolsCount
+		for j := start; j < end; j++ {
+			_ = fmt.Sprintf("%s,id=%v temperature=%v %v", measurementName, id, time.Now().UnixNano(), j)
+		}
+
 		time.Sleep(time.Duration(1) * time.Second)
 	}
 }
